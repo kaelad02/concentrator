55c9db4b556b4db7de14b406416ec2fe6fae04b4
Chris Seieroe <kaelad02@users.noreply.github.com>
2022-01-31 00:47:41 -0800

    addEffect setting

--- src/settings.js
+++ src/settings.js
@@ -1,4 +1,5 @@
 export var debugEnabled;
+export var addEffect;
 
 export const registerSettings = function () {
   game.settings.register("concentrator", "debugLogging", {
@@ -9,6 +10,23 @@ export const registerSettings = function () {
     default: false,
     onChange: (value) => {
       debugEnabled = value;
+    },
+  });
+
+  game.settings.register("concentrator", "addEffect", {
+    name: "When to apply Concentrating",
+    hint: "Always apply after casting a spell with concentration, only if it consumed a resource, or whisper a message to manually apply.",
+    scope: "client",
+    config: true,
+    type: String,
+    choices: {
+      always: "Always",
+      consumed: "Consumed Resource",
+      whisper: "Whisper Message",
+    },
+    default: "always",
+    onChange: (value) => {
+      addEffect = value;
     },
   });
 };
